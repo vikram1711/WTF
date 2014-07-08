d0bc4799e87ca6e9e5bc2ee84731761fee3482d5
vikram1711 <jhatheinttelect@gmail.com>
2014-07-07 21:09:26 -0700

    Updated the pom file to get the correct dependencies 	modified:   pom.xml 	modified:   src/test/java/org/wtf/Test/WebdriverTest.java

--- src/test/java/org/wtf/Test/WebdriverTest.java
+++ src/test/java/org/wtf/Test/WebdriverTest.java
@@ -1,5 +1,8 @@
 package org.wtf.Test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.wtf.core.WTFEnv;
 import org.wtf.core.WTFTest;
@@ -7,7 +10,13 @@ import org.wtf.core.WTFTest;
 public class WebdriverTest extends WTFTest {
 
   @Test(dataProvider = "WEBDRIVER")
-  public void WebdriverTest1(WTFEnv env) {
+  public void webdriverTest1(WTFEnv env) {
+	  WebDriver driver = env.getDriver();
+	  driver.get("http://www.ebay.com");
+	  WebElement schTxtBox = driver.findElement(By.name("_nkw"));
+	  WebElement schTxtBtn = driver.findElement(By.id("gh-btn"));
+	  schTxtBox.sendKeys("iPhone");
+	  schTxtBtn.click();
     
   }
 }
